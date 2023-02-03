5df46a875b729fcfa01d9f8ad462944af05bb1e5
Hugo <hugo.**@ens-paris-saclay.fr>
2023-02-03 17:00:45 +0100

    New listener

--- src/main/scala/events/Listener.scala
+++ src/main/scala/events/Listener.scala
@@ -5,6 +5,27 @@ import sfml.window.Event
 
 trait Listener {
 
-  def event(e : Event) : Unit
+  def onClosed(e : Event.Closed) : Unit = {}
+  def onResized(e : Event.Resized) : Unit = {}
+  def onLostFocus(e : Event.LostFocus) : Unit = {}
+  def onGainedFocus(e : Event.GainedFocus) : Unit = {}
+  def onTextEntered(e : Event.TextEntered) : Unit = {}
+  def onKeyPressed(e : Event.KeyPressed) : Unit = {}
+  def onKeyReleased(e : Event.KeyReleased) : Unit = {}
+  def onMouseWheelScrolled(e : Event.MouseWheelScrolled) : Unit = {}
+  def onMouseButtonPressed(e : Event.MouseButtonPressed) : Unit = {}
+  def onMouseButtonReleased(e : Event.MouseButtonReleased) : Unit = {}
+  def onMouseMoved(e : Event.MouseMoved) : Unit = {}
+  def onMouseEntered(e : Event.MouseEntered) : Unit = {}
+  def onMouseLeft(e : Event.MouseLeft) : Unit = {}
+  def onJoystickButtonPressed(e : Event.JoystickButtonPressed) : Unit = {}
+  def onJoystickButtonReleased(e : Event.JoystickButtonReleased) : Unit = {}
+  def onJoystickMoved(e : Event.JoystickMoved) : Unit = {}
+  def onJoystickConnected(e : Event.JoystickConnected) : Unit = {}
+  def onJoystickDisconnected(e : Event.JoystickDisconnected) : Unit = {}
+  def onTouchBegan(e : Event.TouchBegan) : Unit = {}
+  def onTouchMoved(e : Event.TouchMoved) : Unit = {}
+  def onTouchEnded(e : Event.TouchEnded) : Unit = {}
+  def onSensorChanged(e : Event.SensorChanged) : Unit = {}
 
 }
