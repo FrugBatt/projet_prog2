3a245ce93ab71fdac6024756b808b78b2422c2a2
Slukoo <simon.lukowski@hotmail.com>
2023-02-10 18:25:19 +0100

    added other directions

--- src/main/scala/objects/King.scala
+++ src/main/scala/objects/King.scala
@@ -17,11 +17,11 @@ class King extends AnimatedGameObject("game/king.png", 16, 17, Array(2,8,8)) {
     var moveright = false
 
     override def update(): Unit = {
-        super.update()
-        if(moveup) {
-
-            this.position = (this.position.x, this.position.y - 1)
-        }
+        super.update() 
+        if(moveup) this.position = (this.position.x, this.position.y - 1)
+        if(moveleft) this.position = (this.position.x - 1, this.position.y)
+        if(movedown) this.position = (this.position.x, this.position.y + 1)
+        if(moveright) this.position = (this.position.x + 1 , this.position.y)
     }
 
     override def onKeyPressed(e : Event.KeyPressed) : Unit = {
@@ -29,11 +29,26 @@ class King extends AnimatedGameObject("game/king.png", 16, 17, Array(2,8,8)) {
             moveup = true
             animationState = 2
         }
+        if (e.code == Keyboard.Key.KeyQ) {
+            moveleft = true
+            animationState = 1
+        }
+        if (e.code == Keyboard.Key.KeyS) {
+            movedown = true
+            animationState = 2
+        }
+        if (e.code == Keyboard.Key.KeyD) {
+            moveright = true
+            animationState = 1
+        }
     }
 
      override def onKeyReleased(e : Event.KeyReleased) : Unit = {
-        if (e.code == Keyboard.Key.KeyZ) {
+        if (e.code == Keyboard.Key.KeyZ || e.code == Keyboard.Key.KeyQ || e.code == Keyboard.Key.KeyS || e.code == Keyboard.Key.KeyD ) {
             moveup = false
+            moveleft = false
+            movedown = false
+            moveright = false
             animationState = 0
         }
     }
