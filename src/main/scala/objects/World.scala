d222b66081ab3a9d0da2c73b90cb83b0d16c2909
Slukoo <simon.lukowski@hotmail.com>
2023-02-10 17:50:11 +0100

    added world

---
+++ src/main/scala/objects/World.scala
@@ -0,0 +1,16 @@
+package prog2
+package objects
+
+import graphics.ResourceManager
+
+import sfml.window.Event
+import sfml.graphics.Rect
+import sfml.graphics.Sprite
+import sfml.graphics.RenderTarget
+import sfml.graphics.RenderStates
+
+class World() extends GameObject("game/terrain.png") {
+    
+    def update() : Unit = {}
+    
+}
