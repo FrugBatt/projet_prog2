167c672e4fce1edf574511d098d06e50285905c0
Slukoo <simon.lukowski@hotmail.com>
2023-02-14 15:38:51 +0100

    reverting the reversion

--- src/main/scala/game/Game.scala
+++ src/main/scala/game/Game.scala
@@ -11,8 +11,10 @@ object Game {
 
   var window : RenderWindow = _
   var scene : Scene = _
+  var width : Int = 1024
+  var height : Int = 768
   def init() : Unit = {
-    window = RenderWindow(VideoMode(1024, 768), "Le RTS de Hugo et Simon les bews")
+    window = RenderWindow(VideoMode(width, height), "Le RTS de Hugo et Simon les bews")
     scene = VoidScene()
   }
 
