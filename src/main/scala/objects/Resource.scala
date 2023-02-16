b99fe6bc6e69219c96f552bb9caa56802e39b4f0
Slukoo <simon.lukowski@hotmail.com>
2023-02-16 09:17:21 +0100

    added resources class and textures

---
+++ src/main/scala/objects/Resource.scala
@@ -0,0 +1,16 @@
+package prog2
+package objects
+
+import sfml.window.Event
+import sfml.graphics.Rect
+import sfml.graphics.RenderTarget
+import sfml.graphics.RenderStates
+import sfml.graphics.Sprite
+
+import graphics.ResourceManager
+
+class Resource(resource : String, val value: Int, val kind: String) extends GameObject(resource){
+}
+
+
+
