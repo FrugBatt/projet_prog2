fc73a4a658db86438c1a13202676c453d25ded83
Slukoo <simon.lukowski@hotmail.com>
2023-02-20 18:47:00 +0100

    added hpbar update upon attack'

--- src/main/scala/objects/Enemy.scala
+++ src/main/scala/objects/Enemy.scala
@@ -29,7 +29,6 @@ class Enemy() extends AnimatedGameObject("game/enemy.png", 18, 22, Array(2), ani
 
     override def attack(dmg : Int) = {
         hp = (hp - dmg).max(0)
-        if(hpbar.isDefined) println(hpbar.get.state)
         if (hp == 0) return AttackKilled()
         return AttackSuccess()
     }
