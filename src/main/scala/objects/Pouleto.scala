ccd13474922e3c882329182ac3d23bddc360429c
Slukoo <simon.lukowski@hotmail.com>
2023-03-10 17:43:35 +0100

    added fleeing animation for pouletos

--- src/main/scala/objects/Pouleto.scala
+++ src/main/scala/objects/Pouleto.scala
@@ -15,7 +15,7 @@ import objects.Resource
 import events._
 import scene._
 
-class Pouleto(context: Scene) extends EntityGameObject(50, () => new Resource("game/meat.png", 3, ResourceType.MEAT), "game/chicken.png", 16, 17, Array(12)) {
+class Pouleto(context: Scene) extends EntityGameObject(5, () => new Resource("game/meat.png", 3, ResourceType.MEAT), "game/chicken.png", 16, 17, Array(8,8,8),50L) {
     
     var roaming = false
     var fleeing : Option[SpriteGameObject] = None
@@ -31,10 +31,15 @@ class Pouleto(context: Scene) extends EntityGameObject(50, () => new Resource("g
         if (fleeing.isDefined) {
             val x = (this.position.x - fleeing.get.position.x)
             val y = (this.position.y - fleeing.get.position.y)
-            val norm : Float = 5*((java.lang.Math.sqrt(x*x + y*y)).toFloat)
+            if (x > 0) state = 1
+            else state = 2
+            val norm : Float = 3*((java.lang.Math.sqrt(x*x + y*y)).toFloat)
             context.safe_move(this, x/norm, y/norm)
 
-            if (System.currentTimeMillis() - fleeing_since > 3000L) fleeing = None
+            if (System.currentTimeMillis() - fleeing_since > 4000L) {
+                fleeing = None
+                state = 0
+            }
         }
 
     }
