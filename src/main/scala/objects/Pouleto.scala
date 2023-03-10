2a9444f52284cf8c897f832cbff5cf01e9913864
Slukoo <simon.lukowski@hotmail.com>
2023-03-10 18:07:35 +0100

    pouleto now roam randomly when not fleeing

--- src/main/scala/objects/Pouleto.scala
+++ src/main/scala/objects/Pouleto.scala
@@ -1,6 +1,8 @@
 package prog2
 package objects
 
+import scala.math._
+
 import graphics.ResourceManager
 
 import sfml.window.Keyboard
@@ -17,7 +19,10 @@ import scene._
 
 class Pouleto(context: Scene) extends EntityGameObject(5, () => new Resource("game/meat.png", 3, ResourceType.MEAT), "game/chicken.png", 16, 17, Array(8,8,8),50L) {
     
-    var roaming = false
+    val rand = new scala.util.Random
+    var roaming : Option[Vector2[Float]] = None
+    var roaming_since : Long = 0L
+    var roaming_time : Long = rand.nextLong(5000L)
     var fleeing : Option[SpriteGameObject] = None
     var fleeing_since : Long = 0L
 
@@ -33,13 +38,34 @@ class Pouleto(context: Scene) extends EntityGameObject(5, () => new Resource("ga
             val y = (this.position.y - fleeing.get.position.y)
             if (x > 0) state = 1
             else state = 2
-            val norm : Float = 3*((java.lang.Math.sqrt(x*x + y*y)).toFloat)
+            val norm : Float = 3*((sqrt(x*x + y*y)).toFloat)
             context.safe_move(this, x/norm, y/norm)
 
             if (System.currentTimeMillis() - fleeing_since > 4000L) {
                 fleeing = None
                 state = 0
             }
+        }
+        else if (roaming.isDefined) {
+
+            context.safe_move(this, roaming.get.x/5, roaming.get.y/5)
+            if (roaming.get.x > 0) state = 1
+            else state = 2
+            
+        }
+        if (System.currentTimeMillis() - roaming_since > roaming_time) roam()
+
+    }
+
+    def roam() = {
+        roaming_since = System.currentTimeMillis()
+        if(rand.nextInt(2) == 1) {
+            roaming = None
+            state = 0
+        }
+        else {
+            val angle = rand.nextFloat()*6.3f
+            roaming = Some(Vector2(cos(angle).toFloat,sin(angle).toFloat))
         }
 
     }
