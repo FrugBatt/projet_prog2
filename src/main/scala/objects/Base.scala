290993027fccce0e754ac0e079ad0f2675a4b2b5
Slukoo <simon.lukowski@hotmail.com>
2023-03-27 11:40:07 +0200

    castle texture

--- src/main/scala/objects/Base.scala
+++ src/main/scala/objects/Base.scala
@@ -1,10 +1,11 @@
 package prog2
 package objects
 
+import sfml.graphics.Rect
 
 
-class Base(x: Float, y: Float) extends StatedGameObject("index.png", 64, 64){
-    position.x = x
-    position.y = y
+class Base() extends AnimatedGameObject("game/castle.png", 51, 48,Array(8)){
+    
+    override def collision_box = Some(Rect[Float](position.x + 1, position.y + 24, 47, 23))
 
 }
