5fee4b93fddaf34fcaa2af0437e588cce90868c6
Slukoo <simon.lukowski@hotmail.com>
2023-04-08 01:15:17 +0200

    fixed a bug that could make objects spawn within other objects

--- src/main/scala/objects/Goblin.scala
+++ src/main/scala/objects/Goblin.scala
@@ -9,7 +9,7 @@ import sfml.system.Vector2
 
 class Goblin(context: Scene) extends Enemy[Base](context,10, () => new Resource("game/coin.png", 2, ResourceType.COIN),"game/goblin.png", 16, 18, Array(5,8,8,8,8), animationTime = 100L, hp_x_offset = 3, target_id = 2){
 
-    override def collision_box = Some(Rect[Float](position.x + 10, position.y, 18, sprite.textureRect.height))
+    override def collision_box = Some(Rect[Float](position.x + 3, position.y, 10, sprite.textureRect.height))
     override def trigger_box = Some(Rect[Float](position.x - 3, position.y - 3, sprite.textureRect.width + 6, sprite.textureRect.height + 6))
 
 }
