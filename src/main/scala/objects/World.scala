407ba8273ea62618dec6a4964f3ecb4c17d8f58d
Hugo <frug.batt.dev@gmail.com>
2023-05-12 16:44:29 +0200

    first step infinite map

--- src/main/scala/objects/World.scala
+++ src/main/scala/objects/World.scala
@@ -9,11 +9,32 @@ import sfml.graphics.Sprite
 import sfml.graphics.RenderTarget
 import sfml.graphics.RenderStates
 
-class World() extends SpriteGameObject("game/terrain.png") {
+class World() extends GameObject() {
+
+  var resource : String = "game/terrain.png"
+  var sprites : Vector[SpriteGameObject] = Vector()
+
+  var width : Int = 1
 
   override def collision_box = None
   override def trigger_box = None
-    
+  
+  def load_sprite(name : String) : Sprite = {
+    ResourceManager.load_resource(resource)
+    return ResourceManager.get_sprite(resource)
+  }
+
+  override def init() : Unit = {
+
+  }
+
   def update() : Unit = {}
     
+  override def draw(target : RenderTarget, states : RenderStates) : Unit = {
+    sprites.foreach(_.draw(target, states))
+  }
+
+  override def close() : Unit = {
+  }
+
 }
