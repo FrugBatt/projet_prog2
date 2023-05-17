4fee466c0a214c706e527b1b59463b13e934b0ce
Hugo <frug.batt.dev@gmail.com>
2023-05-17 13:35:08 +0200

    fix selection box

--- src/main/scala/objects/SelectionBox.scala
+++ src/main/scala/objects/SelectionBox.scala
@@ -17,68 +17,60 @@ import events._
 import scala.compiletime.ops.long
 
 object SelectionBox extends RectangleShapeGameObject(Color(0, 80, 255, 100),0,0,0,0) {
-    var x = 0
-    var y = 0
-    var width = 0
-    var height = 0
-    var clicking = false
-    val zoom = GameScene.camera.zoom
-    var window_height : Int = 0
-    var window_width : Int = 0
+    
+  var mouse_position = Vector2[Float](0,0)
 
-    override def init(): Unit = {
+  var selection_origin = Vector2[Float](0,0)
 
-        super.init()
+  var clicking = false
 
-        Control.leftClick.addListener(click)
+  override def init(): Unit = {
+    super.init()
+    Control.leftClick.addListener(click)
+  }
 
+  override def close(): Unit = {
+    super.close()
+    Control.leftClick.removeListener(click)
+  }
+
+  override def update(): Unit = {
+    super.update()
+
+    shape = { 
+      val r = RectangleShape((mouse_position.x - selection_origin.x, mouse_position.y - selection_origin.y))
+      r.position = selection_origin
+      r.fillColor = Color(255, 0, 40, 100)
+      r.outlineColor = Color(255, 0, 40, 255)
+      r.outlineThickness = 0.5f
+      r
     }
+  }
 
-    override def close(): Unit = {
+  override def draw(target : RenderTarget, states : RenderStates) : Unit = {
+    if(clicking) shape.draw(target, states)
+  }
 
-        super.close()
-        
-        Control.leftClick.removeListener(click)
-
+  def click(press: Boolean) = {
+    if(press){
+      clicking = true
+      selection_origin = mouse_position
+      GameScene.objects.foreach(o => if(o.isInstanceOf[PlayerControlledEntity]) o.asInstanceOf[PlayerControlledEntity].unselect())
+    } else {
+      clicking = false
+      GameScene.trigger_all(Some(Rect[Float](selection_origin.x,selection_origin.y,shape.size.x,shape.size.y)), objs => objs.foreach(o => if(o.isInstanceOf[PlayerControlledEntity]) o.asInstanceOf[PlayerControlledEntity].select()))
     }
+  }
 
-    override def update(): Unit = {
-        super.update()
-        window_width = Game.window.size.x
-        window_height = Game.window.size.y
+  def getView(x : Int) : View = {
+    if (Game.single) GameScene.cameras(0).v
+    else if (x < Game.window.size.x/2) GameScene.cameras(0).v
+    else GameScene.cameras(1).v
+  }
 
+  override def onMouseMoved(e : Event.MouseMoved) = {
+    mouse_position = Game.window.mapPixelToCoords(Vector2(e.x, e.y), getView(e.x))
+  }
 
-
-        width = GameScene.mouse_position().x.toInt - x 
-        height = GameScene.mouse_position().y.toInt - y
-        shape = { 
-            val r = RectangleShape((width,height))
-            r.position = Vector2(x,y)
-            r.fillColor = Color(255, 0, 40, 100)
-            r.outlineColor = Color(255, 0, 40, 255)
-            r.outlineThickness = 0.5f
-            r
-        }
-    }
-
-    override def draw(target : RenderTarget, states : RenderStates) : Unit = {
-        if(clicking) shape.draw(target, states)
-    }
-
-    def click(press: Boolean) = {
-        if(press){
-
-            clicking = true
-            x = GameScene.mouse_position().x.toInt
-            y = GameScene.mouse_position().y.toInt
-            GameScene.objects.foreach(o => if(o.isInstanceOf[PlayerControlledEntity]) o.asInstanceOf[PlayerControlledEntity].unselect())
-        }
-        else{
-
-            clicking = false
-            GameScene.trigger_all(Some(Rect[Float](x,y,width,height)), objs => objs.foreach(o => if(o.isInstanceOf[PlayerControlledEntity]) o.asInstanceOf[PlayerControlledEntity].select()))
-        }
-    }
-
-    }
+}
 
